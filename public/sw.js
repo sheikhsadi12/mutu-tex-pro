bd7a7761c75911f36bf3c00bd1b5855fddf75adc
sheikhsadi12 <136808252+sheikhsadi12@users.noreply.github.com>
2026-04-10 06:17:12 +0600

    Refactor service worker event listeners

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,13 @@
 const CACHE_NAME = 'mutu-tex-cache-v1';
-const urlsToCache = [ '/', '/index.html', '/manifest.json' ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(['/']))
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
